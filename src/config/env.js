c88221eefff94401bc2aa86979e37524554dcd1a
PonPrasanth10 <ponprasanthr@gmail.com>
2026-05-09 19:00:21 +0530

    frontend completed

--- src/config/env.js
+++ src/config/env.js
@@ -4,7 +4,7 @@ const config = {
     API_BASE_URL: 'http://localhost:5000/api'
   },
   production: {
-    API_BASE_URL: '/api' // Assumes frontend and backend are served from same domain
+    API_BASE_URL: 'https://website-analyzer-backend-vo9a.onrender.com/api'
   }
 };
 
